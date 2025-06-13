1370ea3841fca12c962dd7a3f62613bc2a2e6f98
Azeez Gafar <azegaf@gmail.com>
2025-06-13 07:20:33 +0100

    feat: add validation for first and last name in ContactForm
    Adds minimum and maximum length validation to the first and last name fields in the ContactForm using Yup. The first and last name fields must be between 2 and 25 characters long.

--- src/components/sections/ContactForm.tsx
+++ src/components/sections/ContactForm.tsx
@@ -22,8 +22,14 @@ function ContactForm() {
       // _captcha: false,
     },
     validationSchema: Yup.object({
-      firstName: Yup.string().required("First name is required"),
-      lastName: Yup.string().required("Last name is required"),
+      firstName: Yup.string()
+        .min(2, "First name must be at least 2 characters")
+        .max(25, "First name must be at most 25 characters")
+        .required("First name is required"),
+      lastName: Yup.string()
+        .min(2, "Last name must be at least 2 characters")
+        .max(25, "Last name must be at most 25 characters")
+        .required("Last name is required"),
       email: Yup.string().email("Invalid email").required("Email is required"),
       companyName: Yup.string().required("Company name is required"),
       phoneNumber: Yup.string().matches(
